427fe37e7f52ffa34ea848ce165937ce092e73b7
Flavio Alles <flavio.alles@gmail.com>
2016-08-20 20:41:32 -0300

    changing testing

--- backend/bootstrap.rb
+++ backend/bootstrap.rb
@@ -154,15 +154,32 @@ class Customer
   end
 end
 
-# Book Example (build new payments if you need to properly test it)
-foolano = Customer.new
-book = Product.new(name: 'Awesome book', type: :book)
-book_order = Order.new(foolano)
-book_order.add_product(book)
+# create fictional customer
+customer = Customer.new("Joe", "0123456", "01-01-1990")
+# create products container
+products = Hash.new
+# populate products hash
+products[:physical] = Physical.new(name: 'Macbook', price: 5000)
+products[:book] = Book.new(name: 'Learning Ruby', price: 50)
+products[:digital] = Digital.new(name: 'Some Movie', price: 30)
+products[:membership] = Membership.new(name: 'A Newspaper', price: 80)
 
-payment_book = Payment.new(order: book_order, payment_method: CreditCard.fetch_by_hashed('43567890-987654367'))
-payment_book.pay
-p payment_book.paid? # < true
-p payment_book.order.items.first.product.type
+# make one order, payment, and processing for each item in hash
+# open order, add products, collect payment, verify, process
+# order
+order = Order.new(customer)
+products.each do |k,v|
+  order.add_product(v)
+end
 
-# now, how to deal with shipping rules then?
+# payment
+payment = Payment.new(order: order,
+                      payment_method: CreditCard.fetch_by_hashed('43567890-987654367'))
+payment.pay
+
+# process order
+if payment.paid?
+  (payment.order).process
+else
+  puts "Order canceled due to unsucceful payment"
+end
