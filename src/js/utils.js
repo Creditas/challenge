e8babcb91b5b9921a668a65db85b7ee58f9285c5
Enzo Toshio <enzo.toshiba@gmail.com>
2017-01-20 23:51:19 -0200

    adicionando funcoes de manipulacao de dom e integrando com a view

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,8 @@
 export { on };
 export { getElement };
+export { append };
+export { createElement };
+export { createText };
 
 function on(element, event, callback) {
   element.addEventListener(event, callback, true);
@@ -7,4 +10,22 @@ function on(element, event, callback) {
 
 function getElement(element) {
   return document.body.querySelector(element);
+}
+
+function append(container, element) {
+  let containerElement = container;
+
+  if (!(container instanceof HTMLElement)) {
+    containerElement = getElement(container)
+  }
+
+  containerElement.appendChild(element); 
+}
+
+function createElement(tagName) {
+  return document.createElement(tagName);
+}
+
+function createText(text) {
+  return document.createTextNode(text);
 }
--- src/js/view.js
+++ src/js/view.js
@@ -1,14 +1,22 @@
 require('pubsub-js');
 
 import {
-  getElement
+  getElement,
+  append,
+  createElement,
+  createText,
+  on
 } from './utils';
 import { resource } from './resource';
 
 const config = {};
 const user = {};
 const view = {
-  init
+  init,
+  setListeners,
+  getMessage,
+  addMessage,
+  publishMessage
 }
 
 export { view };
@@ -16,6 +24,8 @@ export { view };
 function init(viewConfig) {
   setConfig(viewConfig);
   setListeners();
+
+  Object.assign(user, resource.getUser());
 }
 
 function setListeners() {
@@ -26,4 +36,26 @@ function setListeners() {
 
 function setConfig(viewConfig) {
   Object.assign(config, viewConfig);
+}
+
+function getMessage() {
+  const text = getElement(config.textBox).value;
+
+  return { text, user };
+}
+
+function addMessage(message) {
+  const element = createElement('span');
+  const text = `${message.user.name}: ${message.text}`;
+  const textNode = createText(text);
+
+  append(element, textNode);
+
+  append(config.container, element);
+}
+
+function publishMessage() {
+  const message = getMessage();
+
+  PubSub.publish('message:sent', message);
 }
