d3434a4d0594bd01d70f6fe02fa3ede85ca144c5
Caio Abe <caio.abe@estilo7.com.br>
2017-02-08 23:12:26 -0200

    BEM and JS organization

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,86 +1,86 @@
-// [TO-DO] could have used babel to use const / let / arrow functions safely
-// use strict to avoid hoisting mistakes
 "use strict";
 
-// defining main variables 
-const messagesElementsHolder = document.getElementById('messages-holder'),
-    messagesElements = messagesElementsHolder.getElementsByClassName('messages'),
+// Defining main variables 
+const messagesElementsHolder = document.getElementById('chat'),
+    messagesElements = messagesElementsHolder.getElementsByClassName('chat__messages'),
     newChatButton = document.getElementById('new-chat')
 
 // Messages prototype to create objects
-const Messages = function(messages) {
-    this.messageForm = messages.querySelector('form'),
-    this.simulateDiv = messages.querySelector('.simulate'),
-    this.messageSendButton = this.messageForm.querySelector('button'),
-    this.messageTextInput = this.messageForm.querySelector('input'),
-    this.messageList = messages.querySelector('.message-list')
+const Messages = function (messages) {
+    this.messageForm = messages.querySelector('form')
+    this.simulateDiv = messages.querySelector('.chat__simulate')
+    this.messageSendButton = this.messageForm.querySelector('button')
+    this.messageTextInput = this.messageForm.querySelector('input')
+    this.messageList = messages.querySelector('.chat__message-list')
 }
 
 // Filter new chats and apply listeners to them
 function createChats() {
     Array.from(messagesElements)
-        .filter(messages => messages.classList.contains('messages-new'))
+        .filter(messages => messages.classList.contains('chat__messages--new'))
         .map(messages => {
-        const messageObj = new Messages(messages)
-        messageObj.messageForm.addEventListener('submit', (e) => {
-            e.preventDefault()
-            sendMessage(messageObj, 'me')
+
+            const messageObj = new Messages(messages)
+
+            messageObj.messageForm.addEventListener('submit', (e) => {
+                e.preventDefault()
+                sendMessage(messageObj, 'chat__message-item--me')
+            })
+
+            messageObj.messageSendButton.addEventListener('click', (e) => {
+                e.preventDefault()
+                sendMessage(messageObj, 'chat__message-item--me')
+            })
+
+            messageObj.simulateDiv.addEventListener('click', (e) => {
+                e.preventDefault()
+                let mockMessageObj = messageObj
+                if (messageObj.messageTextInput.value != "")
+                    mockMessageObj.messageTextInput.value = messageObj.messageTextInput.value
+                else
+                    mockMessageObj.messageTextInput.value = "Write something in the input to simulate a bot response =)"
+                sendMessage(messageObj, 'chat__message-item--bot')
+            })
+
+            messages.classList.remove('chat__messages--new')
         })
-        messageObj.messageSendButton.addEventListener('click', (e) => {
-            e.preventDefault()
-            sendMessage(messageObj, 'me')
-        })
-        messageObj.simulateDiv.addEventListener('click', (e) => {
-            e.preventDefault()
-            let mockMessageObj = messageObj
-            if(messageObj.messageTextInput.value != "")
-                mockMessageObj.messageTextInput.value = messageObj.messageTextInput.value
-            else
-                mockMessageObj.messageTextInput.value = "Write something in the input to simulate a bot response =)"
-            sendMessage(messageObj, 'bot')
-        })
-    messages.classList.remove('messages-new')})
 }
 
-// if not empty, add message to the list
+// If not empty, add message to the list
 function sendMessage(messageObj, who) {
     let messageTextInputValue = messageObj.messageTextInput.value
-    if(messageTextInputValue === "") {
-        return
-    }
+
+    if (messageTextInputValue === "") { return }
+
     const liList = messageObj.messageList.querySelectorAll('li'),
         lastLi = liList[liList.length - 1]
+
     let li
-    if(!lastLi.classList.contains(who)) {
+
+    if (!lastLi.classList.contains(who)) {
         li = document.createElement('li')
-        li.classList.add(who)
+        li.classList.add('chat__message-item', who)
         li.innerText = messageObj.messageTextInput.value
         messageObj.messageList.appendChild(li)
     } else {
         li = lastLi
         li.innerHTML += "<br>" + messageObj.messageTextInput.value
     }
-    if(who === 'me') {
+
+    if (who === 'chat__message-item--me') {
         consumeApi(messageTextInputValue, messageObj, sendMessage)
     }
+
     messageObj.messageTextInput.value = ""
     messageObj.messageList.scrollTop = messageObj.messageList.scrollHeight
     messageObj.messageTextInput.focus()
 }
 
-// initiate the first chat
-createChats()
-    // on newChat click add new chat and createChats for new chats 
-    newChatButton.addEventListener('click', function(e) {
-    e.preventDefault()
-    messagesElementsHolder.appendChild(newChat())
-    createChats()
-})
-
 // Create a new chat node
 function newChat() {
-    /* fake bot */ const simulateDiv = document.createElement('div')
-    /* fake bot */ const initialLi = document.createElement('li')
+    /* fake bot */
+    const simulateDiv = document.createElement('div'),
+        initialLi = document.createElement('li')
 
     const messages = document.createElement('div'),
         messageList = document.createElement('ul'),
@@ -89,42 +89,60 @@ function newChat() {
         input = document.createElement('input'),
         button = document.createElement('button')
 
-    /* fake bot */ simulateDiv.classList.add('simulate')
-    /* fake bot */ simulateDiv.innerHTML = "Click here to simulate a bot response"
-    /* fake bot */ initialLi.classList.add('bot')
-    /* fake bot */ initialLi.innerHTML = "Hi! How can I help you?"
+    /* fake bot */
+    simulateDiv.classList.add('chat__simulate')
+    initialLi.classList.add('chat__message-item', 'chat__message-item--bot')
+    simulateDiv.innerHTML = "Click here to simulate a bot response"
+    initialLi.innerHTML = "Hi! How can I help you?"
 
-    messages.classList.add('messages', 'messages-new')
-    messageList.classList.add('message-list')
-    messageInput.classList.add('message-input')
+    messages.classList.add('chat__messages', 'chat__messages--new')
+    messageList.classList.add('chat__message-list')
+    messageInput.classList.add('chat__message-input')
     form.setAttribute('method', 'post')
     input.setAttribute('type', 'text')
     button.setAttribute('type', 'button')
 
-    /* fake bot */ messages.appendChild(simulateDiv)
-    /* fake bot */ messageList.appendChild(initialLi)
+    /* fake bot */
+    messages.appendChild(simulateDiv)
+    messageList.appendChild(initialLi)
 
     messages.appendChild(messageList)
     form.appendChild(input)
     form.appendChild(button)
     messageInput.appendChild(form)
     messages.appendChild(messageInput)
+
     return messages
+
 }
 
 function consumeApi(question, messageObj, fn) {
     var xhr = new XMLHttpRequest()
-    xhr.onreadystatechange = function() {
+
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            // [TO-DO] this have to be verified on server side
             const data = JSON.parse(this.responseText)
-            if(data.hasOwnProperty(question)) {
+            // [TO-DO] this have to be verified on server side
+            if (data.hasOwnProperty(question)) {
                 const mockMessageObj = messageObj
                 mockMessageObj.messageTextInput.value = data[question]
-                sendMessage(mockMessageObj, 'bot')
+
+                sendMessage(mockMessageObj, 'chat__message-item--bot')
             }
         }
     }
+
     xhr.open("GET", "mock.json", true);
+
     xhr.send();
 }
+
+// initiate the first chat
+createChats()
+
+// on newChat click add new chat and createChats for new chats 
+newChatButton.addEventListener('click', function (e) {
+    e.preventDefault()
+    messagesElementsHolder.appendChild(newChat())
+    createChats()
+})
