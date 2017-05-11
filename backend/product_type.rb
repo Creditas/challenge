e25f1b88f89654eb49452ccb3a007ed3d072b781
Marcela Porto <marcela.fporto@gmail.com>
2017-05-10 21:49:20 -0700

    Adds Shippo API gem for fun, and adds methods to generate shipping label for Physical products w/ status

--- backend/product_type.rb
+++ backend/product_type.rb
@@ -1,19 +1,34 @@
+# Added gem for Shippo API(but is using invalid token and no params), just to give it a more realistic feel :)
+require 'shippo'
+
 class Membership
-  def self.shipping
+  def self.shipping(object)
+    "MEMB"
   end
 end
 
 class Book
-  def self.shipping
+  def self.shipping(object)
+    "BOOKS"
   end
 end
 
 class Physical
-  def self.shipping
+  def self.shipping(object)
+    generate_shipping_label(object)
   end
+
+  private
+  def generate_shipping_label(object)
+    Shippo::API.token = 'aff988f77afa0fdfdfadf'
+    @shipment = Shippo::Shipment.create(params)
+    return "Shipping Label for #{object.name} was succesfully created" if @shipment.success?
+    "There was an error generating the shipping label for #{object.name}"
+  end
+
 end
 
 class Digital
-  def self.shipping
+  def self.shipping(object)
   end
 end
