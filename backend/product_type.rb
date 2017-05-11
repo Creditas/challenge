7c7b272da779b7c43f2e3e310860863bebf79680
Marcela Porto <marcela.fporto@gmail.com>
2017-05-10 20:32:25 -0700

    Adds file for Product Type Classes

---
+++ backend/product_type.rb
@@ -0,0 +1,19 @@
+class Membership
+  def self.shipping
+  end
+end
+
+class Book
+  def self.shipping
+  end
+end
+
+class Physical
+  def self.shipping
+  end
+end
+
+class Digital
+  def self.shipping
+  end
+end
