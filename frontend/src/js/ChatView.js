37cb011dc16e2de4de73beac4026ec9b3f5cb104
annanee <annaflavia@fluture.com.br>
2017-06-01 08:54:45 -0300

    Fix indent

--- frontend/src/js/ChatView.js
+++ frontend/src/js/ChatView.js
@@ -2,9 +2,9 @@ class ChatView {
   render(el, id = 0) {
     document.getElementById(el).innerHTML = `<ul class="message-list"></ul>
 
-                    <div class="message-input">
-                      <input type="text" />
-                      <button type="button">Enviar</button>
-                    </div>`;
+      <div class="message-input">
+        <input type="text" />
+        <button type="button">Enviar</button>
+      </div>`;
   }
 }
