c61dc4f01b8efc0fb6ee33579c3082a59cdc2e17
Aurelio Saraiva <aureliosaraiva@gmail.com>
2017-06-21 18:36:00 +0200

    create component chat-window

--- frontend/src/app/services/chat.js
+++ frontend/src/app/services/chat.js
@@ -1,4 +1,26 @@
+import Service from 'ember-service';
+import Evented from 'ember-evented';
 import Ember from 'ember';
 
-export default Ember.Service.extend({
+export default Service.extend(Evented, {
+
+  messages: {
+    'aureliosaraiva-creditas': [{
+      text: 'olá Aurélio'
+    }]
+  },
+  getMessages(chatId) {
+    const key = `messages.${chatId}`;
+
+    // create message store if chatId is undefined
+    if (!this.get(key)) {
+      this.set(key, []);
+    }
+    return this.get(key);
+  },
+  sendMessage(chatId, text){
+    // notify chat-windows that arrive new message
+    this.trigger(`received@${chatId}`, { text });
+    this.get(`messages.${chatId}`).pushObject({ text });
+  }
 });
