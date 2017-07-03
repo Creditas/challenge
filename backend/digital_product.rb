5f86e4066bc7409e709e76346689c2c29065590c
Phelipe Correa <phelipercorrea@gmail.com>
2017-07-02 21:30:54 -0300

    Moved send promotional voucher to Mevie class

--- backend/digital_product.rb
+++ backend/digital_product.rb
@@ -2,6 +2,5 @@ class DigitalProduct < Product
   def buy **options
     super
     p "Sending email to #{options[:customer].name} with details on how to download #{name}"
-    p "Sending promotional 10% OFF voucher to #{options[:customer].name}"
   end
 end
