d94df161221f718f7ed3625a0b99e7dce8fc9623
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-10 18:17:04 -0300

    Add step definitions file

--- backend/features/creditas.feature
+++ backend/features/creditas.feature
@@ -14,7 +14,7 @@ Feature: Payment stream
 
     If payment is a service subscription, you must activate the subscription, and
     notify the user via email about this;
-    
+   
     Given the payment is for a service subscription
     When the payment (for service) is done
     Then we must activate the subscription
@@ -25,17 +25,17 @@ Feature: Payment stream
     If the payment is an ordinary book, you must generate it shipping label with a
     notification that it is a tax-exempt item as provided in the Constitution
     Art. 150, VI, d.
-    
+   
     Given the payment is for ordinary book
     When the payment (for book) is done
     Then we must generate the shipping label with a notification
-    
+   
   Scenario: Digital media
 
     If payment of any digital media (music, video), in addition to sending the
     description of the purchase by email to the buyer, grant a discount voucher of
     $ 10 to the buyer associated with the payment.
-    
+   
     Given the payment is for any "digital" media
     Then we need to send the description of purchase by email to buyer
     And grant a discount voucher of $10 to the buyer
---
+++ backend/features/step_definitions/payment_steps.rb
@@ -0,0 +1,51 @@
+Given(/^the payment is for a physical item$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the payment is done$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^we must generate one shipping label to the shipping box$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the payment is for a service subscription$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the payment \(for service\) is done$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^we must activate the subscription$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^notify the user via email$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the payment is for ordinary book$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the payment \(for book\) is done$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^we must generate the shipping label with a notification$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^the payment is for any "([^"]*)" media$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^we need to send the description of purchase by email to buyer$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^grant a discount voucher of \$(\d+) to the buyer$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
