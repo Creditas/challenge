9407505a83d527a1b55c0ce5943eda5b0564edc6
Vinicius Pereira <vinicius@localhost.localdomain>
2017-08-13 21:48:05 -0300

    feature (types): add BookType class -- add class to control actions for books with OrderItemType as parent

---
+++ backend/types/book_type.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "#{Dir.pwd}/types/order_item_type"
+require_relative "#{Dir.pwd}/services/printer_service"
+
+# Class responsible for handle a book type
+# Parent class is OrderItemType
+class BookType < OrderItemType
+  attr_reader :customer, :product, :item
+
+  def initialize(customer, product, item)
+    super(customer, product, item)
+  end
+
+  def handle
+    @item.shipping_labels.push('Item isento de impostos conforme Art.150, VI, d.')
+
+    PrinterService.print(@item.shipping_labels)
+  end
+end
