fdea011d86164b6041b73edf813de1b3d13d8099
Vinicius Pereira <vinicius@localhost.localdomain>
2017-08-13 21:48:37 -0300

    feature (types): add DigitalType class -- add class to control actions for digital medias with OrderItemType as parent

---
+++ backend/types/digital_type.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "#{Dir.pwd}/types/order_item_type"
+require_relative "#{Dir.pwd}/services/email_service"
+
+# Class responsible for handle a digital type
+# Parent class is OrderItemType
+class DigitalType < OrderItemType
+  attr_reader :customer, :product, :item
+
+  def initialize(customer, product, item)
+    super(customer, product, item)
+  end
+
+  def handle
+    @item.messages.push("Produto Digital adquirido: #{@item.product.name}")
+
+    EmailService.send(@customer.email, @item.messages)
+  end
+end
