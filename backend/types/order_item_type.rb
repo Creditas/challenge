338a43a930d5a1cd6c1a23007bbd98b575384d03
Vinicius Pereira <vinicius@localhost.localdomain>
2017-08-13 21:47:18 -0300

    feature (types): add OrderItemType class -- add a base class for inheritance and control product types

---
+++ backend/types/order_item_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative "#{Dir.pwd}/models/order_item"
+
+# Base class for product types
+# Children classes: BookType, DigitalType, MembershipType, and PhysicalType
+class OrderItemType
+  attr_reader :customer, :product, :item
+
+  def initialize(customer, product, item)
+    @customer = customer
+    @product = product
+    @item = item
+  end
+
+  def handle; end
+end
