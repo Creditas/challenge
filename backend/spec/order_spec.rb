17a878114e22ba92fca53f7a7b7dcebe551bfaad
Paulo Victor Leite Lima Gomes <paulo.gomes@natue.com.br>
2017-09-25 11:27:54 -0300

    test new way of create product and order item

--- backend/spec/order_spec.rb
+++ backend/spec/order_spec.rb
@@ -2,9 +2,9 @@ require 'rspec'
 require 'require_all'
 require_all 'lib'
 
-RSpec.describe Order, "#order" do
+RSpec.describe Order do
   context "order flow" do
-    it "can create full order" do
+    it "can create order with digital product" do
       product = Product.new(name: 'Music: The Unforgiven - Metallica', price: 18.00, type: :digital)
       customer = Customer.new(name: 'Beltrano', email: 'beltrano.silva@creditas.com.br')
       order = Order.new(customer)
@@ -21,7 +21,7 @@ RSpec.describe Order, "#order" do
       expect(order.shipping_address).to eq shipping_address
     end
 
-    it "check handle order item" do
+    it "check if handle order item with digital product" do
       digital = Product.new(name: 'Music: The Unforgiven - Metallica', price: 18.00, type: :digital)
       customer = Customer.new(name: 'Beltrano', email: 'beltrano.silva@creditas.com.br')
       order = Order.new(customer)
@@ -29,6 +29,25 @@ RSpec.describe Order, "#order" do
 
       expect(order.items.first.product.type).to eq :digital
       expect(order.items.first.class).to eq DigitalOrderItem
+    end
+
+    it "should get order price" do
+      beltrano = Customer.new(name: 'Beltrano', email: 'beltrano.silva@creditas.com.br')
+      xbox = ProductFactory::create(name: 'Console Xbox One', price: 1820.00,type: :physical)
+      digital = ProductFactory::create(name: 'Music: The Unforgiven - Metallica', price: 18.00, type: :digital)
+      membership = ProductFactory::create(name: 'Monthly Internet 100MB', price: 87.90,type: :membership)
+      book = ProductFactory::create(name: 'Domain-Driven Design: Tackling Complexity in the Heart of Software', price: 58.00, type: :book)
+
+      order = Order.new(beltrano)
+      order.add_billing_address(Address.new(street: 'Rua bento Salves' ,zipcode: '45678-979'))
+      order.add_shipping_address(Address.new(street: 'Rua Marques de Lages', zipcode: '04162-001'))
+      order.add_product(xbox)
+      order.add_product(membership)
+      order.add_product(digital)
+      order.add_product(book)
+
+
+      expect(order.total_amount).to eq 1983.90
     end
 
   end
--- backend/spec/product_spec.rb
+++ backend/spec/product_spec.rb
@@ -2,11 +2,17 @@ require 'rspec'
 require 'require_all'
 require_all 'lib'
 
-RSpec.describe Product, "#price" do
-  context "with products" do
+RSpec.describe Product do
+  context "in order to check product behavior" do
     it "check correct price" do
       product = Product.new(name: 'Galaxy Edge', price: 1890, type: 'physical')
       expect(product.price).to eq 1890
+    end
+
+    it "check it can create invalid product" do
+      expect {
+        raise ProductFactory.create(name: 'Tesouro SELIC', price: 1500, type: 'public_letter')
+      }.to raise_error(ArgumentError)
     end
   end
 end
