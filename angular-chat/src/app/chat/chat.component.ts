97dc323e5e5154b3fd6c17fdc9b06e8507ca32f1
Vagner <santos.vagnerferreira@gmail.com>
2018-01-14 16:58:44 -0300

    Creating and listening events

--- angular-chat/src/app/chat/chat.component.ts
+++ angular-chat/src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Message } from './Message';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,11 +8,37 @@ import { Message } from './Message';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  conversationHistory: Array<Message> = [];
+  private conversationHistory: Message[]= [];
 
-  constructor() { }
+  constructor(private chatService: ChatService) { }
 
   ngOnInit() {
+    this.getAllMessages();
+    this.watchNewMessages();
   }
 
+  getAllMessages() {
+    console.log(this.chatService.getAll())
+    this.conversationHistory = this.chatService.getAll();
+  }
+
+  watchNewMessages() {
+    this.chatService.onNewMessageIsAvaiable$
+      .subscribe( (messageListUpdated: Message[]) => {
+        this.getAllMessages();
+      });
+  }
+
+  addClientMessage(e) {
+    e.preventDefault();
+    console.log(e)
+
+    let msg = new Message(
+      'teste',
+      new Date(),
+      'client'
+    );
+
+    this.chatService.addMessage(msg);
+  }
 }
