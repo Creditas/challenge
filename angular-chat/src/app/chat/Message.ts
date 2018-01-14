44c8148b15f08524ae399d3bac45c48ea8fabf0f
Vagner <santos.vagnerferreira@gmail.com>
2018-01-14 16:58:11 -0300

    Converting message interface to class

--- angular-chat/src/app/chat/Message.ts
+++ angular-chat/src/app/chat/Message.ts
@@ -1,4 +1,23 @@
-export interface Message {
-  content: string,
-  date: Date
+export class Message {
+  private content: string
+  private date: Date
+  private sender: string
+
+  constructor(content: string, date: Date, sender: string) {
+    this.content = content;
+    this.date = date;
+    this.sender = sender;
+  }
+
+  getContent() {
+    return this.content;
+  }
+
+  getDate() {
+    return this.date;
+  }
+
+  getSender() {
+    return this.sender;
+  }
 }
