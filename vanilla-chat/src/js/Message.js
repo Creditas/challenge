b47e70563ad7913c6c7224c99e7b1e8ca000c179
Vagner <santos.vagnerferreira@gmail.com>
2018-01-14 18:35:09 -0300

    Message class interface

---
+++ vanilla-chat/src/js/Message.js
@@ -0,0 +1,21 @@
+export class Message {
+ 
+    constructor(content, date, sender) {
+      this.content = content;
+      this.date = date;
+      this.sender = sender;
+    }
+  
+    // help methods: getters
+  
+    getContent() {
+      return this.content;
+    }
+  
+    getDate() {
+      return this.date;
+    }
+  
+    getSender() {
+      return this.sender;
+    }
