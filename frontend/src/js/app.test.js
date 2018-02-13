1386649439cd716c83ce331adb525051d8f7f5d2
Rafael Thomazetti <thztti@gmail.com>
2018-02-13 10:50:25 -0200

    Creates util tests

--- frontend/src/js/app.test.js
+++ frontend/src/js/app.test.js
@@ -3,3 +3,11 @@
 var Chat = require('./app.js');
 
 var CreditasChat = new Chat('.messages');
+describe('Utils', function() {
+  it('Gets current time', function() {
+    const date = new Date();
+    const time = `${date.getHours()}h${date.getMinutes() < 10 ? '0' : ''}${date.getMinutes()}`;
+    
+    expect(CreditasChat.getCurrentTime()).toEqual(time);
+  });
+});	
