d8bb319b0bf4712f9bd81c67b17f79fae607de9d
cinthyabrito80 <cinthya_brito@yahoo.com.br>
2018-04-05 20:58:27 -0300

    Removendo informacao no arquivo Env o desafio QA - Cintia Brito

--- qa-test/Creditas_Cintia_Brito/features/support/env.rb
+++ qa-test/Creditas_Cintia_Brito/features/support/env.rb
@@ -10,11 +10,6 @@ require 'openssl'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 #Configurando o driver Capybara
-# Capybara.register_driver :chrome do |app|
-#     Capybara::Selenium::Driver.new(app, :browser => :chrome)
-# end
-
-#Configurando o driver Capybara
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
 end
