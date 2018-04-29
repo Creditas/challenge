bc3bac95ce775e3e2e93b99163c11d268abaa539
Gabriel Mesquita de Araujo <gabrieljustware@gmail.com>
2018-04-29 15:49:09 -0300

    Create shipping class with ship method

---
+++ backend/shipping.rb
@@ -0,0 +1,27 @@
+class Shipping
+  def initialize
+  end
+
+  def ship(payment)
+    payment.order.items.each do |order_item|
+      case order_item.product.type
+        when :physical
+          print_shipping_label(:physical, payment.order.address.zipcode)
+        when :membership
+          order_item.product.activate_customer_membership
+          order_item.send_order_item_description_email_to_customer
+        when :book
+          print_shipping_label(:book, payment.order.address.zipcode)
+        when :digital
+          order_item.send_order_item_description_email_to_customer
+          payment.grant_discount_voucher
+      end
+    end
+  end
+
+  def print_shipping_label(product_type, shipping_address)
+    product_type == :physical ? shipping_address : shipping_address + " Item isento de
+                                                                       impostos conforme disposto na
+                                                                       Constituição Art. 150, VI, d."
+  end
+end
