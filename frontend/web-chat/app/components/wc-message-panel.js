9c02be30782fd196c09364224f3574263fe8bf1f
Silvio Rodrigues <silviorodrigues7@gmail.com>
2018-06-14 19:05:25 -0300

    Send message to store

--- frontend/web-chat/app/components/wc-message-panel.js
+++ frontend/web-chat/app/components/wc-message-panel.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    sendMessage() {
+      let text = this.get('text');
+
+      this.get('store').createRecord('messages', {text});
+
+      this.set('text', '');
+    }
+  }
 });
