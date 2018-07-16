b93823ec03e5ea5cc8f7a3037f71955816e765ad
Pedro Henrique Santiago <pedroh_s_@hotmail.com>
2018-07-16 03:43:21 -0300

    :sparkles: Chat container

--- frontend/chat/src/containers/Chat/index.js
+++ frontend/chat/src/containers/Chat/index.js
@@ -1,10 +1,50 @@
 import React from 'react';
-import Page from '../../components/Page/Page';
+import { connect } from 'react-redux';
 
-const Chat = () => (
+import { send } from '../../modules/messages';
+import { changeMessage } from '../../modules/chats';
+import Page from '../../components/Page/Page';
+import ChatHeading from '../../components/ChatHeading/ChatHeading';
+import MessagesPanel from '../../components/MessagesPanel/MessagesPanel';
+import Message from '../../components/Message/Message';
+import ChatActions from '../../components/ChatActions/ChatActions';
+import Room from '../../components/Room/Room';
+
+const Chat = ({
+  messages,
+  handleSend,
+  handleMessageTyping,
+  message
+}) => (
   <Page>
-    <h1>Chat</h1>
+    <ChatHeading title="Chat" />
+    <Room>
+      <MessagesPanel>
+        {
+          messages.map(({text, id, loading}) => (
+            <Message text={loading ? 'loading...' : text} key={id} />
+          ))
+        }
+      </MessagesPanel>
+      <ChatActions 
+        onSubmitMessage={handleSend} 
+        onMessageTyping={handleMessageTyping}
+        message={message}
+      />
+    </Room>
   </Page>
 )
 
-export default Chat;
+const mapStateToProps = (state) => {
+  return {
+    messages: Object.values(state.messages),
+    message: state.chats.message
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  handleSend: (payload) => dispatch(send(payload)),
+  handleMessageTyping: (payload) => dispatch(changeMessage(payload))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Chat);
