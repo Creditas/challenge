7d71d2626ae471441d893940e57657ad6180f5cd
Pedro Henrique Santiago <pedroh_s_@hotmail.com>
2018-07-16 03:09:37 -0300

    :white_check_mark: TDD Chat Actions

--- frontend/chat/src/components/ChatActions/ChatActions.test.js
+++ frontend/chat/src/components/ChatActions/ChatActions.test.js
@@ -1,14 +1,30 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import ChatActions from './ChatActions'
+
 describe('chatActions', () => {
   it('should has a message field', () => {
-  
+    const wrapper = shallow(<ChatActions />);
+    expect(wrapper.find('.textField')).toHaveLength(1);
   })
   it('should has a send button', () => {
-  
+    const wrapper = shallow(<ChatActions />);
+    expect(wrapper.find('.sendBtn')).toHaveLength(1);
   })
   it('should send message when click button', () => {
-  
+    const sendFunc = jest.fn()
+    const wrapper = shallow(
+      <ChatActions onSubmitMessage={() => sendFunc()} message="mock" />
+    );
+    wrapper.find('.sendBtn').simulate('click')
+    expect(sendFunc).toHaveBeenCalled()
   })
   it('should not send empty message', () => {
-  
+    const sendFunc = jest.fn()
+    const wrapper = shallow(
+      <ChatActions onSubmitMessage={() => sendFunc()} message="" />
+    );
+    wrapper.find('.sendBtn').simulate('click')
+    expect(sendFunc).not.toHaveBeenCalled()
   })
 })
