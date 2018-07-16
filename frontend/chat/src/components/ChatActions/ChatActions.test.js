778c186be00c90f7464e9d2134bbcd6b63ebc767
Pedro Henrique Santiago <pedroh_s_@hotmail.com>
2018-07-16 02:23:22 -0300

    :white_check_mark: TDD Chat Actions

---
+++ frontend/chat/src/components/ChatActions/ChatActions.test.js
@@ -0,0 +1,14 @@
+describe('chatActions', () => {
+  it('should has a message field', () => {
+  
+  })
+  it('should has a send button', () => {
+  
+  })
+  it('should send message when click button', () => {
+  
+  })
+  it('should not send empty message', () => {
+  
+  })
+})
