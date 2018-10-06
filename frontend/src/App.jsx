d50b418960cf249b6ebb7256d0535112a9c32a3d
romulo bordezani <romulo.bordezani@dafiti.com.br>
2018-10-06 15:51:45 -0300

    Adding Babel conf to coexist prettier and airbnb

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.something = true;
+  }
+
   render() {
     return (
       <div className="App">
