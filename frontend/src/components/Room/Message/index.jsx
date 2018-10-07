8d9fa4dc02ee7dd87c1d645e2b1a57cede528987
romulo bordezani <romulo.bordezani@dafiti.com.br>
2018-10-07 19:16:45 -0300

    Final touchs

--- frontend/src/components/Room/Message/index.jsx
+++ frontend/src/components/Room/Message/index.jsx
@@ -3,16 +3,24 @@ import PropTypes from 'prop-types';
 import Grow from '@material-ui/core/Grow';
 
 import styles from './Message.scss';
+import audio from './notification.mp3';
 
-const Message = ({ data }) => {
-  return (
-    <Grow in>
-      <div className={styles.message}>
-        <div className={styles.message__content}>{data.content}</div>
-      </div>
-    </Grow>
-  ); // TODO - Show user info
-};
+// eslint-disable-next-line
+class Message extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <Grow in>
+        <div className={styles.message}>
+          <audio src={audio} autoPlay>
+            <track kind="captions" />
+          </audio>
+          <div className={styles.message__content}>{data.content}</div>
+        </div>
+      </Grow>
+    ); // TODO - Show user info somehow
+  }
+}
 
 Message.propTypes = {
   data: PropTypes.any.isRequired
