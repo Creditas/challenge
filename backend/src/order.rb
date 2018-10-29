891d14902adc0c3448b8b0da3655dfcf9e2706d0
Bruno <bmonteirog@gmail.com>
2018-10-29 03:05:23 -0200

    Remove notes

--- backend/src/order.rb
+++ backend/src/order.rb
@@ -1,31 +1,21 @@
 class Order
-  # Permissão de leitura para essas variáveis
   attr_reader :customer, :items, :payment, :address, :closed_at
 
-  # Construtor
   def initialize(customer, overrides = {})
-    # seta customer
     @customer = customer
-    # define items como array vazio
     @items = []
-    # Define a classe de cada item do Pedido, pode ser sobrescrito
     @order_item_class = overrides.fetch(:item_class) { OrderItem }
-    # Define o endereço de entrega do Pedido, pode ser sobrescrito
     @address = overrides.fetch(:address) { Address.new(zipcode: '45678-979') }
   end
 
-  # Adiciona produto ao Pedido
   def add_product(product)
-    # Array  push
     @items << @order_item_class.new(order: self, product: product)
   end
 
-  # Faz um map nos items e soma o total
   def total_amount
     @items.map(&:total).inject(:+)
   end
 
-  # Finaliza pedido
   def close(closed_at = Time.now)
     @closed_at = closed_at
   end
--- backend/src/payment.rb
+++ backend/src/payment.rb
@@ -1,32 +1,20 @@
 class Payment
-  # Permissão de leitura para essas variáveis
   attr_reader :authorization_number, :amount, :invoice, :order, :payment_method, :paid_at
 
-  # Construtor da Classe, recebe objeto attributes
   def initialize(attributes = {})
-    # seta authorization number e amount
     @authorization_number, @amount = attributes.values_at(:authorization_number, :amount)
-    # seta invoice e order
     @invoice, @order = attributes.values_at(:invoice, :order)
-    # seta payment method
     @payment_method = attributes.values_at(:payment_method)
   end
 
-  # Método pagar, recebe timestamp do pagamento 
   def pay(paid_at = Time.now)
-    # pega o total do pedido
     @amount = order.total_amount
-    # gera numero de autorização
     @authorization_number = Time.now.to_i
-    # gera invoice com endereço de cobrança, endereço de entrega e pedido
     @invoice = Invoice.new(billing_address: order.address, shipping_address: order.address, order: order)
-    # pega argumento e coloca na variavel paid_at
     @paid_at = paid_at
-    # chamada para encerrar o pedido
-    order.close(@paid_at)
+    order.close(@paid_at)    
   end
 
-  # Método que informa se o pagamento está pago ou não
   def paid?
     !paid_at.nil?
   end
