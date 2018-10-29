9816f8e7d6f0a4012d2c1b7b37f85f01f8b5eb58
Bruno Monteiro <bruno.monteiro@justdigital.com.br>
2018-10-29 17:57:34 -0200

    Updates bootstrap file

--- backend/bootstrap.rb
+++ backend/bootstrap.rb
@@ -1,16 +1,29 @@
 require './load'
 
-# Book Example (build new payments if you need to properly test it)
-foolano = Customer.new(name: "Zachery Tissell", address: Address.new(zipcode: '6816'))
-book = Product.new(name: 'Awesome book', type: :book, price: 15, attributes: {})
-book_order = Order.new(foolano)
-book_order.add_product(book)
+customer = Customer.new(name: "Zachery Tissell", address: Address.new(zipcode: '6816'), email: "zachery@email.com")
 
-payment_book = Payment.new(order: book_order, payment_method: CreditCard.fetch_by_hashed('43567890-987654367'))
-payment_book.pay
-p payment_book.paid? # < true
-p payment_book.order.items.first.product.type
+book_product = Product.new(name: 'Homo deus', type: :book, price: 47, attributes: {})
+physical_product = Product.new(name: 'Smartband Tway', type: :physical, price: 189, attributes: {})
+digital_product = Product.new(name: 'Exile On Main Street', type: :digital, price: 10, attributes: {
+    :full_name => "Album: Exile On Main Street (1972)",
+    :artist => "The Rolling Stones"
+})
+membership_product = Product.new(name: 'Wine Club', type: :membership, price: 120, attributes: {})
 
-shipping = Shipping.new(book_order)
+order = Order.new(customer)
+order.add_product(book_product)
+order.add_product(physical_product)
+order.add_product(digital_product)
+order.add_product(membership_product)
+
+payment = Payment.new(order: order, payment_method: CreditCard.fetch_by_hashed('43567890-987654367'))
+payment.pay
+
+shipping = Shipping.new(order)
 shipping.ship
-# now, how to deal with shipping rules then?
+
+shipping.order.items.each do |order_item|
+    print "Shipping log for Product " + order_item.product.name + ":\n"
+    print order_item.shipping_log
+    print "\n\n"
+end
