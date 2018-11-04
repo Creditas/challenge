ad91d54c7ef5d74e683df7328b0f24109b144c46
Caio Zem <caioadz@hotmail.com>
2018-11-04 19:20:45 -0200

    Criando status para determinar se o pagamento foi efetuado

--- backend/lib/entities/payment/payment.rb
+++ backend/lib/entities/payment/payment.rb
@@ -1,18 +1,26 @@
 class Payment
-  attr_reader :authorization_number, :amount, :invoice, :order, :payment_method, :paid_at
+  attr_reader :authorization_number, :amount, :invoice, :order, :payment_method, :paid_at, :status
 
   def initialize(attributes = {})
     @authorization_number, @amount = attributes.values_at(:authorization_number, :amount)
     @invoice, @order = attributes.values_at(:invoice, :order)
     @payment_method = attributes.values_at(:payment_method)
+    @status = :pending
   end
 
   def pay(paid_at = Time.now)
+    order.set_payment(self)
+
     @amount = order.total_amount
     @authorization_number = Time.now.to_i
     @invoice = Invoice.new(billing_address: order.address, shipping_address: order.address, order: order)
     @paid_at = paid_at
-    order.set_payment(self)
+
+    set_status(:complete)
+  end
+
+  def set_status(status)
+    @status = status
   end
 
   def paid?
