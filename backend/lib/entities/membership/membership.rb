fa7d8cb3c07700f2a73b17044b61fbc90043a5b6
Caio Zem <caioadz@hotmail.com>
2018-11-04 22:35:36 -0200

    Implementando Membership e criando evento na sua ativacao

--- backend/lib/entities/membership/membership.rb
+++ backend/lib/entities/membership/membership.rb
@@ -1,3 +1,33 @@
+require 'observer'
+
 class Membership
-  # you can customize this class by yourself
+  include Observable
+
+  attr_reader :product, :customer, :invoice, :activated_at, :status
+
+  def initialize(product, customer, invoice)
+    @Product = product
+    @Customer = customer
+    @Invoice = invoice
+    @Status = :pending
+  end
+
+  def activate(activated_at = Time.now)
+    @activated_at = activated_at;
+
+    set_status(:activated)
+
+    notify
+  end
+
+private
+
+  def notify()
+    changed
+    notify_observers(self)
+  end
+
+  def set_status(status)
+    @status = status
+  end
 end
