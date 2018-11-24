4f38ff137cc6cb971fa8ce855814f941bbf1383d
Andreolle Dantas <andreolle18@gmail.com>
2018-11-24 21:04:45 -0200

    adding default background to home page + adding credits to artist + adding background color to header

--- chatz/src/pages/Home.js
+++ chatz/src/pages/Home.js
@@ -8,6 +8,16 @@ import enter from '@/images/enter.svg'
 import create from '@/images/group.svg'
 
 class Home extends Component {
+	// componentDidMount() {
+	// 	fetch('https://cors.io/?https://www.behance.net/v2/projects/27198827?api_key=8fVWyPdLzJwgectbQq6BOtLCEpnMpF5r', {
+	// 		method: 'GET'
+	// 	})
+	// 	.then(function(response) {
+	// 		response.json().then(data => {
+	// 			console.log(data.project.modules)
+	// 		})
+	// 	})
+	// }
 	render() {
 		return (
 			<div className="home">
@@ -24,6 +34,9 @@ class Home extends Component {
 						</Link>
 					</Card>
 				</div>
+
+				<h5 className="home__artcredit">Artist:
+				<a href="https://www.behance.net/sarahedith" target="_blank">Sarah Edith</a></h5>
 			</div>
 		)
 	}
