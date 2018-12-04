9af637cd663c9ffd8a3859f5ace9878a43718dda
Diego Peres <speres.diego@gmail.com>
2018-12-04 21:25:32 -0200

    Refactoring customer

---
+++ backend/customer.py
@@ -0,0 +1,18 @@
+
+
+class Customer:
+    @property
+    def address(self):
+        return dict(zip_code=self._data.get('zip_code'), address=self._data['address'])
+
+    def __init__(self, _id):
+        ''' Get all informations (data) from database. '''
+        self._data = data
+
+    def get_order(self, products=None):
+        ''' Get the opened order in database if doesn't exist create a new one. '''
+        if not order:
+            order = Order.create({'customer': self, 'products': products})
+        elif products:
+            order.update({'products': products})
+        return order
