f6dc8ee5458f8ad5081a9bee9ae60ec35f3b1f2b
Diego Peres <speres.diego@gmail.com>
2018-12-04 22:18:47 -0200

    Removing main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from customer import Customer
-from payment import Payment, CARD, BANK_TRANSFER
-
-
-customer = Customer('customer-id')
-order = customer.get_order(products=['product-id-1', 'product-id-2', 'product-id-3'])
-payment = Payment.create({'order': order})
-payment.pay(CARD)
