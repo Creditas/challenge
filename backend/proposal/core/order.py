fd9057ee43e177dacf86b84adc02b76a82421ce7
Kersul <joaohkfaria@gmail.com>
2019-03-19 19:13:39 -0300

    feat(core): Implementing Order with select the OrderItem class

--- backend/proposal/core/order.py
+++ backend/proposal/core/order.py
@@ -1,6 +1,11 @@
 import time
 from core.order_item import OrderItem
+from core.order_item_book import OrderItemBook
+from core.order_item_digital import OrderItemDigital
+from core.order_item_membership import OrderItemMembership
+from core.order_item_physical import OrderItemPhysical
 from core.address import Address
+from core.product import Product
 
 class Order:
     customer = None
@@ -12,11 +17,23 @@ class Order:
     def __init__(self, customer, attributes={}):
         self.customer = customer
         self.items = []
-        self.order_item_class = attributes.get('order_item_class', OrderItem)
         self.address = attributes.get('address', Address(zipcode='45678-979'))
+    
+    def __get_order_item_class(self, product):
+        if product.type == Product.BOOK:
+            return OrderItemBook
+        elif product.type == Product.DIGITAL:
+            return OrderItemDigital
+        elif product.type == Product.MEMBERSHIP:
+            return OrderItemMembership
+        elif product.type == Product.PHYSICAL:
+            return OrderItemPhysical
+        # Invalid type
+        return None
 
     def add_product(self, product):
-        self.items.append(self.order_item_class(order=self, product=product))
+        order_item_class = self.__get_order_item_class(product)
+        self.items.append(order_item_class(order=self, product=product))
 
     def total_amount(self):
         total = 0
@@ -26,6 +43,8 @@ class Order:
         return total
 
     def close(self, closed_at=time.time()):
+        # Dispatching for each OrderItem
+        for item in self.items:
+            item.dispatch()
+        # Closing
         self.closed_at = closed_at
-
-    # remember: you can create new methods inside those classes to help you create a better design
