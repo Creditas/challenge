7d6246779f77df2e8c0df8efaf328f99cd61b49c
Mauricio Jr <mauricio.junior@hubfintech.com.br>
2019-05-21 16:20:34 -0300

    Testa classe especialista para entregar livro

---
+++ backend/spec/shipping/shipping_book_spec.rb
@@ -0,0 +1,24 @@
+
+require_relative '../../lib/config/load'
+
+describe Shipping::ShippingBook do
+  describe "Shipping::ShippingItem" do
+    before do
+      foolano = Customer.new
+      @book = Product.new(name: 'Awesome book', type: :book)
+      @physical = Product.new(name: 'Notebook', type: :physical)
+      @digital = Product.new(name: 'Audio Book', type: :digital)
+      @membership = Product.new(name: 'Subscription Book', type: :membership)
+      @order = Order.new(foolano)
+      @order.add_product(@book)
+      @opts = {
+        Obs: 'Produto isento de impostos conforme disposto na Constituição Art. 150, VI, d'  
+      }
+    end
+
+    it 'generate correct shipping label book item' do
+      expect(Printer::ShippingLabel).to receive(:new).with(@order.customer,@order.address, @opts).and_call_original
+      Shipping::ShippingBook.new(@order.items.first).dispatch_now
+    end
+  end
+end
