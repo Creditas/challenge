b0452b78b5a12a390c0750e95b8a906f5bdea379
Mauricio Jr <mauricio.junior@hubfintech.com.br>
2019-05-21 16:24:43 -0300

    Testa classe especialista para entregar produtos fisicos

---
+++ backend/spec/shipping/shipping_physical.rb
@@ -0,0 +1,22 @@
+
+require_relative '../../lib/config/load'
+
+describe Shipping::ShippingPhysical do
+  describe "Shipping::ShippingItem" do
+    before do
+      foolano = Customer.new
+      @book = Product.new(name: 'Awesome book', type: :book)
+      @physical = Product.new(name: 'Notebook', type: :physical)
+      @digital = Product.new(name: 'Audio Book', type: :digital)
+      @membership = Product.new(name: 'Subscription Book', type: :membership)
+      @order = Order.new(foolano)
+      @order.add_product(@book)
+
+    end
+
+    it 'generate correct shipping label book item' do
+      expect(Printer::ShippingLabel).to receive(:new).with(@order.customer,@order.address).and_call_original
+      Shipping::ShippingPhysical.new(@order.items.first).dispatch_now
+    end
+  end
+end
