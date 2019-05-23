7d27241b389dbcd7981e625c9751d3811aeee9eb
Fabiana <frontfabi@gmail.com>
2019-05-23 17:13:52 -0300

    :zap: Front-End Challenge - helpers

---
+++ frontend/src/helpers/elements.js
@@ -0,0 +1,13 @@
+export const el = {
+  form: document.querySelector('.form'),
+  warrantyRange: document.querySelector('[name="valor-garantia-range"]'),
+  warrantyInput: document.querySelector('[name="valor-garantia"]'),
+  warrantyMin: document.getElementById('min-garantia'),
+  loanRange: document.getElementById('valor-emprestimo-range'),
+  loanInput: document.getElementById('valor-emprestimo'),
+  warrantySelect: document.getElementById('garantia'),
+  timeSelect: document.getElementById('prazo'),
+  totalValue: document.getElementById('total'),
+  quotaValue: document.getElementById('valor-parcela'),
+  rateValue: document.getElementById('juros')
+}
---
+++ frontend/src/helpers/values.js
@@ -0,0 +1,17 @@
+export const optionValues = {
+  'Veículo': {
+    valorMin: 3000,
+    valorMax: 100000,
+    prazo: [24, 36, 48]
+  },
+  'Imóvel': {
+    valorMin: 30000,
+    valorMax: 4500000,
+    prazo: [120, 180, 240]
+  }
+}
+export const commonValues = {
+  juros: 2.34,
+  iof: 6.38,
+  valorEmprestimo: 0.8
+}
