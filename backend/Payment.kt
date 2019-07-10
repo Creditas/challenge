4e5defa1ab1936ad67174f33011e5b87fa5125b4
Alvaro Albiach <alvaro.albiach@gmail.com>
2019-07-11 01:58:26 +0200

    First refactor

---
+++ backend/Payment.kt
@@ -0,0 +1,11 @@
+interface PaymentMethod
+
+data class CreditCard(val number: String) : PaymentMethod
+
+data class Payment(val order: Order, val paymentMethod: PaymentMethod) {
+    val paidAt = Date()
+    val authorizationNumber = paidAt.time
+    val amount = order.totalAmount
+
+    val invoice = Invoice(order)
+}
