75a28e436608237f8187d4a3c0ff192a5babd899
Estela Medrano <stlmedrano@gmail.com>
2019-07-10 17:01:27 +0200

    generates a shipping label if payment is for a physical item

--- backend/bootstrap.rb
+++ backend/bootstrap.rb
@@ -1,13 +1,18 @@
 class Payment
-  attr_reader :authorization_number, :amount, :invoice, :order, :payment_method, :paid_at
+  attr_reader :authorization_number, :amount, :invoice, :order, :payment_method, :paid_at, :result
 
   def initialize(attributes = {})
     @authorization_number, @amount = attributes.values_at(:authorization_number, :amount)
-    @invoice, @order = attributes.values_at(:invoice, :order)
+    @invoice, @order, @result = attributes.values_at(:invoice, :order, :result)
     @payment_method = attributes.values_at(:payment_method)
   end
 
   def pay(paid_at = Time.now)
+    @result = Result.new
+    if  order.items.first.product.type == :physical 
+      @result.generate_for_shipping
+    end
+    
     @amount = order.total_amount
     @authorization_number = Time.now.to_i
     @invoice = Invoice.new(billing_address: order.address, shipping_address: order.address, order: order)
@@ -18,7 +23,18 @@ class Payment
   def paid?
     !paid_at.nil?
   end
+
 end
+
+class Result
+
+  def shipping_label?
+    true
+  end
+  def generate_for_shipping
+  end
+end
+
 
 class Invoice
   attr_reader :billing_address, :shipping_address, :order
--- backend/spec/payment_spec.rb
+++ backend/spec/payment_spec.rb
@@ -16,4 +16,18 @@ describe 'payment' do
         expect(payment_book.order.items.first.product.type).to be :book
     end
 
+    it 'generates a shipping label if payment is for a physical item' do
+        physical = Product.new(type: :physical, name: 'Awesome physical')
+        customer = Customer.new
+        physical_order = Order.new(customer)
+        credit_card = CreditCard.fetch_by_hashed('43567890-987654367')
+        generate = true
+
+        physical_order.add_product(physical)
+        payment_physical = Payment.new(order: physical_order, payment_method: credit_card)
+        payment_physical.pay
+
+        expect(payment_physical.result.shipping_label?).to be generate
+    end
+
 end
