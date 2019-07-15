f551f83a666aa52b9753212196ee8fdfa1813a5f
Alvaro Albiach <alvaro.albiach@housers.com>
2019-07-15 14:25:58 +0200

    Dependency Inyection

--- backend/src/main/kotlin/challenge/OrderItemRules.kt
+++ backend/src/main/kotlin/challenge/OrderItemRules.kt
@@ -11,47 +11,57 @@ import challenge.service.SubscriptionService
 sealed class OrderItemRules {
     abstract fun process(order: Order, orderItem: OrderItem)
 
-    internal object ProductPhysicalRule : OrderItemRules() {
+    internal class ProductPhysicalRule(
+        private val printerService: PrinterService
+    ) : OrderItemRules() {
         override fun process(order: Order, orderItem: OrderItem) {
             println("- Processing ProductPhysicalRule")
-            PrinterService.instance().printLabel(order, orderItem)
+            printerService.printLabel(order, orderItem)
         }
     }
 
-    internal object ProductMembershipRule : OrderItemRules() {
+    internal class ProductBookRule(
+        private val printerService: PrinterService
+    ) : OrderItemRules() {
+        override fun process(order: Order, orderItem: OrderItem) {
+            println("- Processing ProductBookRule")
+            printerService.printTaxExceptionLabel(order, orderItem)
+        }
+    }
+
+    internal class ProductDigitalRule(
+        private val emailService: EmailService,
+        private val discountService: DiscountService
+    ) : OrderItemRules() {
+        override fun process(order: Order, orderItem: OrderItem) {
+            println("- Processing ProductDigitalRule")
+            emailService.sendPurchaseDescription(order, orderItem)
+
+            discountService
+                .grantDiscount(order, 10)
+                .let { emailService.sendDiscountVoucher(it) }
+        }
+    }
+
+    internal class ProductMembershipRule(
+        private val emailService: EmailService,
+        private val subscriptionService: SubscriptionService
+    ) : OrderItemRules() {
         override fun process(order: Order, orderItem: OrderItem) {
             println("- Processing ProductMembershipRule")
 
-            SubscriptionService.instance()
+            subscriptionService
                 .subscribe(order, orderItem)
-                .let { EmailService.instance().sendSubscription(it) }
-        }
-    }
-
-    internal object ProductBookRule : OrderItemRules() {
-        override fun process(order: Order, orderItem: OrderItem) {
-            println("- Processing ProductBookRule")
-            PrinterService.instance().printTaxExceptionLabel(order, orderItem)
-        }
-    }
-
-    internal object ProductDigitalRule : OrderItemRules() {
-        override fun process(order: Order, orderItem: OrderItem) {
-            println("- Processing ProductDigitalRule")
-            EmailService.instance().sendPurchaseDescription(order, orderItem)
-
-            DiscountService.instance()
-                .grantDiscount(order, 10)
-                .let { EmailService.instance().sendDiscountVoucher(it) }
+                .let { emailService.sendSubscription(it) }
         }
     }
 }
 
 class OrderRulesFactory {
     fun getInstance(orderItem: OrderItem): OrderItemRules = when (orderItem.product.type) {
-        ProductType.PHYSICAL -> OrderItemRules.ProductPhysicalRule
-        ProductType.BOOK -> OrderItemRules.ProductBookRule
-        ProductType.DIGITAL -> OrderItemRules.ProductDigitalRule
-        ProductType.MEMBERSHIP -> OrderItemRules.ProductMembershipRule
+        ProductType.PHYSICAL -> OrderItemRules.ProductPhysicalRule(PrinterService.instance)
+        ProductType.BOOK -> OrderItemRules.ProductBookRule(PrinterService.instance)
+        ProductType.DIGITAL -> OrderItemRules.ProductDigitalRule(EmailService.instance, DiscountService.instance)
+        ProductType.MEMBERSHIP -> OrderItemRules.ProductMembershipRule(EmailService.instance, SubscriptionService.instance)
     }
 }
