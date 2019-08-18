afc3f1df2bbb148b4a2686911cc996b9bc8e9d1c
Angelo Reale <angelo.reale@gmail.com>
2019-08-17 22:52:19 -0300

    Removing uneccessary declarations.

--- frontend/src/js/page-simulation/page-simulation.js
+++ frontend/src/js/page-simulation/page-simulation.js
@@ -1,11 +1,5 @@
 import '../../scss/page-simulation.scss'
 import { Form } from './form'
-import { Helpers } from './helpers/helpers'
-
-var MIN_WARRANTY = 3750.0
-var MAX_WARRANTY = 125000.0
-var MIN_LOAN = 3000.0
-var MAX_LOAN = 8000.0
 
 export default class CreditasChallenge {
   static initialize() {
