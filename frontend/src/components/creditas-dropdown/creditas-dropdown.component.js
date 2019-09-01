045b4c0ec46136f5f27b9fc948dee3ab3c1378d2
rmngrc <ramon@dred.com>
2019-09-01 22:41:31 +0200

    Adding creditas-dropdown

---
+++ frontend/src/components/creditas-dropdown/creditas-dropdown.component.js
@@ -0,0 +1,64 @@
+import './creditas-dropdown.component.css'
+
+export class CreditasDropdown extends HTMLElement {
+  /**
+   * Gets the tag of the web-component.
+   *
+   * @readonly
+   * @static
+   * @memberof CreditasDropdown
+   */
+  static get tag () {
+    return 'creditas-dropdown'
+  }
+
+  /**
+   * Is invoked each time the custom element is appended into
+   * a document-connected element.
+   *
+   * @memberof CreditasDropdown
+   */
+  connectedCallback () {
+    this.render()
+    this.setEventListeners()
+  }
+
+  /**
+   * Sets all event listeners.
+   *
+   * @memberof CreditasDropdown
+   */
+  setEventListeners () {
+    this.$dropdown = this.querySelector('.creditas-dropdown__select')
+
+    this.$dropdown.addEventListener('change', (event) => {
+      this.dispatchEvent(new CustomEvent(`creditas-dropdown:changed`, {
+        bubbles: true,
+        detail: {
+          label: this.labelFor,
+          value: event.target.value
+        }
+      }))
+    })
+  }
+
+  /**
+   * Renders the component.
+   *
+   * @memberof CreditasDropdown
+   */
+  render () {
+    this.labelFor = this.getAttribute('label-for')
+    this.labelText = this.getAttribute('label-text')
+    this.dropdownValues = this.getAttribute('dropdown-values').trim().split(',')
+
+    this.innerHTML = `
+      <div class="creditas-dropdown">
+        <label class="creditas-dropdown__label" for="${this.labelFor}">${this.labelText}</label>
+        <select class="creditas-dropdown__select" name="${this.labelFor}" id="${this.labelFor}" required>
+          ${this.dropdownValues.map((value) => `<option value="${value}">${value}</option>`)}
+        </select>
+      </div>
+    `
+  }
+}
---
+++ frontend/src/components/creditas-dropdown/creditas-dropdown.component.test.js
@@ -0,0 +1,73 @@
+import { CreditasDropdown } from './creditas-dropdown.component'
+import { TestUtils } from '@test/utils'
+
+describe('CreditasDropdown component', () => {
+  beforeAll(() => {
+    window.customElements.define(CreditasDropdown.tag, CreditasDropdown)
+  })
+
+  it('should render the label with the right text and for property', async () => {
+    const el = await TestUtils.render(CreditasDropdown.tag, {
+      'label-for': 'for',
+      'label-text': 'Text!',
+      'dropdown-values': '25,50,100'
+    })
+
+    const label = el.querySelector('label')
+
+    expect(label.getAttribute('for')).toBe('for')
+    expect(label.innerText.trim()).toBe('Text!')
+  })
+
+  it('should render the select element with the right options', async () => {
+    const el = await TestUtils.render(CreditasDropdown.tag, {
+      'label-for': 'for',
+      'label-text': 'Text!',
+      'dropdown-values': '25,50,100'
+    })
+
+    const select = el.querySelector('select')
+
+    expect(select.getAttribute('id')).toBe('for')
+    expect(select.getAttribute('name')).toBe('for')
+  })
+
+  it('should render the right options within the select element', async () => {
+    const el = await TestUtils.render(CreditasDropdown.tag, {
+      'label-for': 'for',
+      'label-text': 'Text!',
+      'dropdown-values': '25,50,100'
+    })
+
+    const options = el.querySelectorAll('option')
+
+    expect(options.length).toBe(3)
+    expect(options[0].getAttribute('value')).toBe('25')
+    expect(options[0].innerText.trim()).toBe('25')
+    expect(options[1].getAttribute('value')).toBe('50')
+    expect(options[1].innerText.trim()).toBe('50')
+    expect(options[2].getAttribute('value')).toBe('100')
+    expect(options[2].innerText.trim()).toBe('100')
+  })
+
+  it('should dispatch event', async (done) => {
+    const el = await TestUtils.render(CreditasDropdown.tag, {
+      'label-for': 'for',
+      'label-text': 'Text!',
+      'dropdown-values': '25,50,100'
+    })
+
+    // Listenening for changes
+    document.addEventListener('creditas-dropdown:changed', (e) => {
+      expect(e.bubbles).toBe(true)
+      expect(e.detail.label).toBe('for')
+      expect(e.detail.value).toBe('50')
+      done();
+    })
+
+    // Triggering change event on select.
+    const select = el.querySelector('select')
+    select.value = '50'
+    select.dispatchEvent(new Event('change'))
+  })
+})
