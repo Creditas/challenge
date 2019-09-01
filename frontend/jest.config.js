af8798b3f2a36a2a7fd5fb9ea974998f0f7022bc
rmngrc <ramon@dred.com>
2019-09-01 13:13:05 +0200

    Using save-exact and moving config to separate file

---
+++ frontend/jest.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  browser: true,
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
+  testEnvironment: 'jest-environment-jsdom',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
+  testMatch: [
+    '**/__tests__/**/*.js?(x)',
+    '**/?(*.)+(spec|test).js?(x)'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  setupTestFrameworkScriptFile: '<rootDir>/config/setupTest.js',
+  transform: {
+    '.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+  }
+}
