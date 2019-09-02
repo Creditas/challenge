e2d887c5f234705a27750510548aa66e600cde3c
rmngrc <ramon@dred.com>
2019-09-02 19:43:50 +0200

    Adding comments

--- frontend/src/components/creditas-footer/creditas-footer.component.js
+++ frontend/src/components/creditas-footer/creditas-footer.component.js
@@ -2,14 +2,32 @@ import './creditas-footer.component.css'
 import template from './creditas-footer.component.html'
 
 export class CreditasFooter extends HTMLElement {
+  /**
+   * Gets the tag of the web-component.
+   *
+   * @readonly
+   * @static
+   * @memberof CreditasFooter
+   */
   static get tag () {
     return 'creditas-footer'
   }
 
+  /**
+   * Is invoked each time the custom element is appended into
+   * a document-connected element.
+   *
+   * @memberof CreditasFooter
+   */
   connectedCallback () {
     this.render()
   }
 
+  /**
+   * Renders the component.
+   *
+   * @memberof CreditasFooter
+   */
   render () {
     this.innerHTML = template
   }
--- frontend/src/components/creditas-header/creditas-header.component.js
+++ frontend/src/components/creditas-header/creditas-header.component.js
@@ -2,14 +2,32 @@ import './creditas-header.component.css'
 import template from './creditas-header.component.html'
 
 export class CreditasHeader extends HTMLElement {
+  /**
+   * Gets the tag of the web-component.
+   *
+   * @readonly
+   * @static
+   * @memberof CreditasHeader
+   */
   static get tag () {
     return 'creditas-header'
   }
 
+  /**
+   * Is invoked each time the custom element is appended into
+   * a document-connected element.
+   *
+   * @memberof CreditasHeader
+   */
   connectedCallback () {
     this.render()
   }
 
+  /**
+   * Renders the component.
+   *
+   * @memberof CreditasHeader
+   */
   render () {
     this.innerHTML = template
   }
