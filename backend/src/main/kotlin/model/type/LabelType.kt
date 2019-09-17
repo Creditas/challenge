da2980647603404b72c00eaf31870044be61bf5d
Taís Guerra <tais_guerra@terceiros.sicredi.com.br>
2019-09-17 08:49:35 -0300

    ADD model classes.

---
+++ backend/src/main/kotlin/model/type/LabelType.kt
@@ -0,0 +1,6 @@
+package challenge.model.type
+
+enum class LabelType(val message: String) {
+    TAX_FREE("Item isento de impostos conforme disposto na Constituição Art. 150, VI, d."),
+    PHYSICAL("Item físico.")
+}
