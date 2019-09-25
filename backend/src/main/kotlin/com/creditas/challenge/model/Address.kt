c8c97591cc333da6b31fdcafb6536ba17524c4dc
Bruno <bruno.ariev@gmail.com>
2019-09-25 04:19:36 -0300

    including builder pattern for Address

--- backend/src/main/kotlin/com/creditas/challenge/model/Address.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Address.kt
@@ -4,4 +4,26 @@ data class Address(val country: String,
                    val streetAddress: String,
                    val city: String,
                    val state: String,
-                   val postalCode: String)
+                   val postalCode: String) {
+
+    class Builder {
+        private lateinit var country: String
+        private lateinit var streetAddress: String
+        private lateinit var city: String
+        private lateinit var state: String
+        private lateinit var postalCode: String
+
+        fun country(country: String) = apply { this.country = country.trim() }
+
+        fun streetAddress(streetAddress: String) = apply { this.streetAddress = streetAddress.trim() }
+
+        fun city(city: String) = apply { this.city = city.trim() }
+
+        fun state(state: String) = apply { this.state = state.trim() }
+
+        fun postalCode(postalCode: String) = apply { this.postalCode = postalCode.trim() }
+
+        fun build() = Address(country, streetAddress, city, state, postalCode)
+    }
+
+}
