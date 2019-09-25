e025d091b12da1205c53d9da54d82273f55c39f4
Bruno <bruno.ariev@gmail.com>
2019-09-25 03:34:48 -0300

    incl. subtotal calc for Item

--- backend/src/main/kotlin/com/creditas/challenge/model/Item.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Item.kt
@@ -1,3 +1,7 @@
 package com.creditas.challenge.model
 
-data class Item(val product: Product, val quantity: Int) { }
+data class Item(val product: Product, val quantity: Int) {
+
+    val subtotal = product.price.multiply(quantity.toBigDecimal())
+
+}
