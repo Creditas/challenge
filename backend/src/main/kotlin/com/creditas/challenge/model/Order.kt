5c771f612b318c16bdf17613ef7ed98c092f7c33
Bruno <bruno.ariev@gmail.com>
2019-09-25 09:19:29 -0300

    defining methods to place(), pay(), fulfill() and complete() orders

--- backend/src/main/kotlin/com/creditas/challenge/model/Order.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Order.kt
@@ -1,18 +1,96 @@
 package com.creditas.challenge.model
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 interface Order {
 
-    val items: List<Item>
     val account: Account
+    val items: List<Item>
+    val feesAndDiscounts: Map<String, BigDecimal>
+    var paymentMethod: PaymentMethod?
+
+    fun selectPaymentMethod(paymentMethod: PaymentMethod) = apply {
+        this.paymentMethod = paymentMethod
+    }
+
+    fun place()
+
+    fun pay()
+
+    fun invoice()
+
+    fun fulfillment()
+
+    fun complete()
+
+    fun subtotal(): BigDecimal {
+        return items.asSequence()
+            .map { item -> item.subtotal }
+            .fold(BigDecimal.ZERO) { acc, value ->acc.plus(value) }
+            .setScale(2, RoundingMode.HALF_UP)
+    }
+
+    fun grandTotal(): BigDecimal {
+        return feesAndDiscounts.values.asSequence()
+            .fold(BigDecimal.ZERO) { acc, value ->acc.plus(value) }
+            .plus(subtotal())
+            .setScale(2, RoundingMode.HALF_UP)
+    }
 }
 
 class PhysicalOrder(override val items: List<Item>,
                     override val account: Account) : Order {
 
+    override val feesAndDiscounts = HashMap<String, BigDecimal>()
+    override var paymentMethod: PaymentMethod? = account.getDefaultPaymentMethod()
+
+    override fun place() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pay() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun invoice() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun fulfillment() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun complete() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
 
 class DigitalOrder(override val items: List<Item>,
                    override val account: Account) : Order {
+
+    override val feesAndDiscounts = HashMap<String, BigDecimal>()
+    override var paymentMethod: PaymentMethod? = account.getDefaultPaymentMethod()
+
+    override fun place() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pay() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun invoice() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun fulfillment() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun complete() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
 
@@ -20,5 +98,28 @@ class MembershipOrder(override val items: List<Item>,
                       override val account: Account) : Order {
 
     constructor(item: Item, account: Account): this(listOf(item), account)
+
+    override val feesAndDiscounts = HashMap<String, BigDecimal>()
+    override var paymentMethod: PaymentMethod? = account.getDefaultPaymentMethod()
+
+    override fun place() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pay() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun invoice() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun fulfillment() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun complete() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
