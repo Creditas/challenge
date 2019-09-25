3e6ff85927fd08c7faeeb3eb89aba12649374c7d
Bruno <bruno.ariev@gmail.com>
2019-09-25 05:06:30 -0300

    incl. methods to handle incl/deleting address from addressBook

--- backend/src/main/kotlin/com/creditas/challenge/model/Account.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Account.kt
@@ -3,10 +3,15 @@ package com.creditas.challenge.model
 data class Account(val name: String, val email: String, private val clrPassword: String) {
 
     val password: String by lazy { encryptPassword(clrPassword) }
-    val addressBook = mutableListOf<Address>()
+    val addressBook = mutableSetOf<Address>()
+
+    fun addAddress(address: Address) = apply { addressBook.add(address) }
+
+    fun deleteAddress(address: Address) = apply { addressBook.remove(address) }
 
     private fun encryptPassword(clearTextPassword: String): String {
-        TODO("encrypt password with SHA-256/512 encryption")
+        //TODO("encrypt password with SHA-256/512 encryption")
+        return clearTextPassword.hashCode().toString()
     }
 
 }
