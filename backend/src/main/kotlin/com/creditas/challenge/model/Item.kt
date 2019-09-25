fce705f0c6b8e2ae906f01f762e6aaa5189e49a8
Bruno <bruno.ariev@gmail.com>
2019-09-25 03:40:19 -0300

    Incl. validations for Product and Item

--- backend/src/main/kotlin/com/creditas/challenge/model/Item.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Item.kt
@@ -4,4 +4,7 @@ data class Item(val product: Product, val quantity: Int) {
 
     val subtotal = product.price.multiply(quantity.toBigDecimal())
 
+    init {
+        require(quantity > 0) { "Quantity must be > 0" }
+    }
 }
--- backend/src/main/kotlin/com/creditas/challenge/model/Product.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Product.kt
@@ -5,6 +5,11 @@ import java.math.RoundingMode
 
 data class Product(val name: String, val type: ProductType, private val _price: Double) {
 
+    init {
+        require(name.isNotBlank()) { "Product name cannot be blank" }
+        require(_price > 0.0) { "Produce price must be > 0" }
+    }
+
     val price: BigDecimal = _price
         .toBigDecimal()
         .setScale(2, RoundingMode.HALF_UP)
