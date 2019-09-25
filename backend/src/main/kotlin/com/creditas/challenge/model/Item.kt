89626a13c90d8a2ff763f461cd51c6c2df9cf4ae
Bruno <bruno.ariev@gmail.com>
2019-09-25 06:16:34 -0300

    incl. addMore and updateTo function to Item and their unit tests

--- backend/src/main/kotlin/com/creditas/challenge/model/Item.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Item.kt
@@ -1,10 +1,19 @@
 package com.creditas.challenge.model
 
-data class Item(val product: Product, val quantity: Int) {
+data class Item(val product: Product, var quantity: Int) {
 
     val subtotal = product.price.multiply(quantity.toBigDecimal())
 
     init {
-        require(quantity > 0) { "Quantity must be > 0" }
+        require(quantity > 0) { "Quantity must be greaterThan 0" }
+    }
+
+    fun addMore(quantity: Int) = apply {
+        require(quantity > 0) { "Quantity must be greaterThan 0" }
+        this.quantity += quantity }
+
+    fun updateTo(quantity: Int) = apply {
+        require(quantity >= 0) { "Quantity must be equalTo or greaterThan 0" }
+        this.quantity = quantity
     }
 }
--- backend/src/test/kotlin/com/creditas/challenge/model/ItemTest.kt
+++ backend/src/test/kotlin/com/creditas/challenge/model/ItemTest.kt
@@ -8,10 +8,47 @@ internal class ItemTest {
 
     @Test
     fun `when quantity is lower than or equalTo 0, throw IllegalArgEx`() {
-        assertThrows(IllegalArgumentException::class.java) {
+        val ex = assertThrows(IllegalArgumentException::class.java) {
             val product = Product("product", ProductType.PHYSICAL, 1.99)
             Item(product, 0)
         }
+    }
+
+    @Test
+    fun `when addMore is called, if qtty is greaterThanZero, it should increment the quantity`() {
+        val product = Product("product", ProductType.PHYSICAL, 1.99)
+        val item = Item(product, 10)
+        item.addMore(1)
+        assertThat(item.quantity).isEqualTo(11)
+    }
+
+    @Test
+    fun `when addMore is called, if qtty is lowerThanOrEqualToZero, it should throw IllegalArgEx`() {
+        val product = Product("product", ProductType.PHYSICAL, 1.99)
+        val item = Item(product, 10)
+        val ex = assertThrows(IllegalArgumentException::class.java) {
+            item.addMore(-1)
+        }
+        assertThat(ex.message).isEqualTo("Quantity must be greaterThan 0")
+    }
+
+
+    @Test
+    fun `when updateTo is called, if qtty is greaterOrEqualToZero, it should overwrite the quantity`() {
+        val product = Product("product", ProductType.PHYSICAL, 1.99)
+        val item = Item(product, 10)
+        item.updateTo(1)
+        assertThat(item.quantity).isEqualTo(1)
+    }
+
+    @Test
+    fun `when updateTo is called, if qtty is lowerThanZero, it should throw IllegalArgEx`() {
+        val product = Product("product", ProductType.PHYSICAL, 1.99)
+        val item = Item(product, 10)
+        val ex = assertThrows(IllegalArgumentException::class.java) {
+            item.updateTo(-1)
+        }
+        assertThat(ex.message).isEqualTo("Quantity must be equalTo or greaterThan 0")
     }
 
     @Test
