fb00b7e2bfaf37089df38a5be42eb4ef9aea9acc
Bruno <bruno.ariev@gmail.com>
2019-09-25 05:26:09 -0300

    incl. validations for account and TODOs

--- backend/src/main/kotlin/com/creditas/challenge/model/Account.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/Account.kt
@@ -5,6 +5,12 @@ data class Account(val name: String, val email: String, private val clrPassword:
     val password: String by lazy { encryptPassword(clrPassword) }
     val addressBook = mutableSetOf<Address>()
 
+    init {
+        require(name.isNotEmpty()) { "Name cannot be blank" }
+        validatePassword(clrPassword)
+        validateEmail(email)
+    }
+
     fun addAddress(address: Address) = apply { addressBook.add(address) }
 
     fun deleteAddress(address: Address) = apply { addressBook.remove(address) }
@@ -12,6 +18,16 @@ data class Account(val name: String, val email: String, private val clrPassword:
     private fun encryptPassword(clearTextPassword: String): String {
         //TODO("encrypt password with SHA-256/512 encryption")
         return clearTextPassword.hashCode().toString()
+    }
+
+    private fun validatePassword(password: String) {
+        //TODO("include rules for password")
+        require(password.isNotEmpty()) { "Password must not be blank" }
+    }
+
+    private fun validateEmail(email: String) {
+        //TODO("include regex validation for email")
+        require(email.isNotEmpty()) { "Invalid email address" }
     }
 
 }
