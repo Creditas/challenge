f96695e2a7f9128201d172380d04ad94aafaae24
Bruno <bruno.ariev@gmail.com>
2019-09-25 07:12:24 -0300

    impl. add, updateQuantity, delete and subtotal on ShoppingCart

--- backend/src/main/kotlin/com/creditas/challenge/model/ShoppingCart.kt
+++ backend/src/main/kotlin/com/creditas/challenge/model/ShoppingCart.kt
@@ -1,25 +1,45 @@
 package com.creditas.challenge.model
 
+import java.lang.IllegalArgumentException
 import java.math.BigDecimal
+import java.math.RoundingMode
+import java.util.function.BiFunction
 
 class ShoppingCart {
 
     val items = HashMap<Product, Item>()
 
-    fun add(product: Product, quantity: Int): ShoppingCart {
-        TODO("implement add")
+    fun add(product: Product, quantity: Int) = apply {
+        items.compute(product, BiFunction { _, item ->
+            item?.addMore(quantity) ?:
+            Item(product, quantity)
+        })
     }
 
-    fun updateQuantity(product: Product, quantity: Int): ShoppingCart {
-        TODO("implement update")
+    fun updateQuantity(product: Product, quantity: Int) = apply {
+        if (quantity == 0) {
+            delete(product)
+        } else {
+            items.compute(product, BiFunction { _, item ->
+                item?.updateTo(quantity) ?:
+                throw IllegalArgumentException("Product specified is not in the Cart")
+            })
+        }
     }
 
-    fun delete(product: Product): ShoppingCart {
-        TODO("implement delete")
+    fun delete(product: Product) = apply {
+        if (items.containsKey(product)) {
+            items.remove(product)
+        } else {
+            throw IllegalArgumentException("Product specified is not in the Cart")
+        }
     }
 
     fun subtotal(): BigDecimal {
-        TODO("implement subtotal")
+        return items.values.asSequence()
+            .map { item -> item.subtotal }
+            .fold(BigDecimal.ZERO) { acc, value ->acc.plus(value) }
+            .setScale(2, RoundingMode.HALF_UP)
     }
 
     fun checkout(): Order {
