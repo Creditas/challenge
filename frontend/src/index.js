3126b191cc8d076022c99ad6200bc3a387dc4fe5
Andy <andreza392@gmail.com>
2019-12-17 00:37:32 -0300

    creditas challenge frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,71 +1,127 @@
-import './styles.css'
+import './styles/main.css'
 
 export const checkFormValidity = formElement => formElement.checkValidity()
-
-export const getFormValues = formElement =>
-  Object.values(formElement.elements)
-    .filter(element => ['SELECT', 'INPUT'].includes(element.nodeName))
-    .map(element => ({
-      field: element.name,
-      value: element.value
-    }))
-
-export const toStringFormValues = values => {
-  const match = matchString => value => value.field === matchString
-  const IOF = 6.38 / 100
-  const INTEREST_RATE = 2.34 / 100
-  const TIME = values.find(match('parcelas')).value / 1000
-  const VEHICLE_LOAN_AMOUNT = values.find(match('valor-emprestimo')).value
-
-  return `Confirmação\n${values
-    .map(value => `Campo: ${value.field}, Valor: ${value.value}`)
-    .join('\n')}`.concat(
-      `\nTotal ${(IOF + INTEREST_RATE + TIME + 1) * VEHICLE_LOAN_AMOUNT}`
-    )
-}
-
-export function Send(values) {
-  return new Promise((resolve, reject) => {
-    try {
-      resolve(toStringFormValues(values))
-    } catch (error) {
-      reject(error)
-    }
-  })
-}
 
 export function Submit(formElement) {
   formElement.addEventListener('submit', function (event) {
     event.preventDefault()
-    if (checkFormValidity(formElement)) {
-      Send(getFormValues(formElement))
-        .then(result => confirm(result, 'Your form submited success'))
-        .catch(error => Alert('Your form submited error', error))
+    return alert('Solicitação enviada com sucesso!')
+  })
+}
+
+function calculateResult() {
+  const IOF = (6.38 / 100)
+  const interestRate = (2.34 / 100)
+  const quota = document.getElementById('parcelas').value
+  const loanAmount = document.getElementById('valor-emprestimo').value
+  const time = (quota / 1000)
+  const total = (IOF + interestRate + time + 1) * loanAmount
+  const totalQuota = total / quota
+
+  document.getElementById('quota').innerHTML = totalQuota.toFixed(2)
+  document.getElementById('amount').innerHTML = 'R$ ' + total.toFixed(2)
+}
+
+function handleChangeRangeUnderWarranty() {
+  const warrantyRangeElement = document.getElementById('valor-garantia-range')
+  const warrantyElement = document.getElementById('valor-garantia')
+  const warrantyType = document.getElementById('garantia')
+  const minWarrantyTitle = document.getElementById('min-garantia')
+  const maxWarrantyTitle = document.getElementById('max-garantia')
+  const quota = document.getElementById('parcelas')
+
+  warrantyRangeElement.min = 12000.0
+  warrantyRangeElement.max = 24000.0
+  minWarrantyTitle.innerHTML = 'R$ 12.000'
+  maxWarrantyTitle.innerHTML = 'R$ 24.000'
+
+  warrantyType.addEventListener('change', function (event) {
+    if(warrantyType.value == 'veiculo') {
+      warrantyRangeElement.min = 12000.0
+      warrantyRangeElement.max = 24000.0
+      minWarrantyTitle.innerHTML = 'R$ 12.000'
+      maxWarrantyTitle.innerHTML = 'R$ 24.000'
+      warrantyElement.value = 12000
+
+      quota[0].value = 24
+      quota[0].innerHTML = 24
+      quota[1].value = 36
+      quota[1].innerHTML = 36
+      quota[2].value = 48
+      quota[2].innerHTML = 48
+    }
+
+    if(warrantyType.value == 'imovel') {
+      warrantyRangeElement.min = 150000.0
+      warrantyRangeElement.max = 8500000.0
+      minWarrantyTitle.innerHTML = 'R$ 150.000'
+      maxWarrantyTitle.innerHTML = 'R$ 8.500.000'
+      warrantyElement.value = 150000
+
+      quota[0].value = 120
+      quota[0].innerHTML = 120
+      quota[1].value = 180
+      quota[1].innerHTML = 180
+      quota[2].value = 240
+      quota[2].innerHTML = 240
     }
   })
-}
 
-export function handleChangeRangeVehicleUnderWarranty(
-  warrantyRangeElement,
-  vehicleWarrantyElement
-) {
-  const MIN_VALUE = 12000.0
   warrantyRangeElement.addEventListener('change', function (event) {
-    vehicleWarrantyElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
+    warrantyElement.value = warrantyRangeElement.value
+  })
+
+  warrantyElement.addEventListener('input', function (event) {
+    warrantyRangeElement.value = warrantyElement.value
+  })
+
+  quota.addEventListener('change', function (event) {
+    calculateResult()
   })
 }
 
-export function handleChangeVehicleLoanAmount(
-  loanAmountRangeElement,
-  loanAmountElement
-) {
-  const MIN_VALUE = 30000.0
+function handleChangeLoanAmount() {
+  const loanAmountRangeElement = document.getElementById('valor-emprestimo-range')
+  const loanAmountElement = document.getElementById('valor-emprestimo')
+  const warrantyType = document.getElementById('garantia')
+  const minLoanAmountTitle = document.getElementById('min-emprestimo')
+  const maxLoanAmountTitle = document.getElementById('max-emprestimo')
+
+  minLoanAmountTitle.innerHTML = 'R$ 3.000'
+  maxLoanAmountTitle.innerHTML = 'R$ 100.000'
+  loanAmountRangeElement.min = 3000.0
+  loanAmountRangeElement.max = 100000.0
+
+  warrantyType.addEventListener('change', function (event) {
+    console.log(warrantyType.value)
+    if(warrantyType.value == 'veiculo') {
+      loanAmountRangeElement.min = 3000.0
+      loanAmountRangeElement.max = 100000.0
+      minLoanAmountTitle.innerHTML = 'R$ 3.000'
+      maxLoanAmountTitle.innerHTML = 'R$ 100.000'
+      loanAmountElement.value = 3000
+    }
+
+    if(warrantyType.value == 'imovel') {
+      loanAmountRangeElement.min = 30000.0
+      loanAmountRangeElement.max = 4500000.0
+      minLoanAmountTitle.innerHTML = 'R$ 30.000'
+      maxLoanAmountTitle.innerHTML = 'R$ 4.500.000'
+      loanAmountElement.value = 30000
+    }
+  })
+
   loanAmountRangeElement.addEventListener('change', function (event) {
-    loanAmountElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
+    loanAmountElement.value = loanAmountRangeElement.value
+    calculateResult()
+  })
+
+  loanAmountElement.addEventListener('input', function (event) {
+    loanAmountRangeElement.value = loanAmountElement.value
+    calculateResult()
   })
 }
+
 
 export default class CreditasChallenge {
   static initialize() {
@@ -75,15 +131,10 @@ export default class CreditasChallenge {
   static registerEvents() {
     Submit(document.querySelector('.form'))
 
-    handleChangeRangeVehicleUnderWarranty(
-      document.getElementById('valor-garantia-range'),
-      document.getElementById('valor-garantia')
-    )
+    handleChangeRangeUnderWarranty()
+    handleChangeLoanAmount()
 
-    handleChangeVehicleLoanAmount(
-      document.getElementById('valor-emprestimo-range'),
-      document.getElementById('valor-emprestimo')
-    )
+    calculateResult()
   }
 }
 
