4df6b6db15a4c2c836cccb001411a22d095a6349
Isabela Silva <eu.silvaisabela@gmail.com>
2019-12-22 13:40:57 -0300

    Slipt form fields functions

---
+++ frontend/src/form/__fields/form__fields.js
@@ -0,0 +1,11 @@
+import './form__fields.css'
+
+const getFormValues = formElement =>
+  Object.values(formElement.elements)
+    .filter(element => ['SELECT', 'INPUT'].includes(element.nodeName))
+    .map(element => ({
+      field: element.name,
+      value: element.value
+    }))
+
+export default getFormValues
