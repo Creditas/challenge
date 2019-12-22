39640424b3991857274ca39bd6fd26fc49d6cd7d
Isabela Silva <eu.silvaisabela@gmail.com>
2019-12-22 13:43:26 -0300

    Slipt form functions to new file

--- frontend/src/form/form.js
+++ frontend/src/form/form.js
@@ -2,17 +2,20 @@ import './form.css'
 
 export const checkFormValidity = formElement => formElement.checkValidity()
 
-export const toStringFormValues = values => {
+function calculationLendingVehicle (values) {
   const match = matchString => value => value.field === matchString
   const IOF = 6.38 / 100
   const INTEREST_RATE = 2.34 / 100
   const TIME = values.find(match('parcelas')).value / 1000
   const VEHICLE_LOAN_AMOUNT = values.find(match('valor-emprestimo')).value
+  return (IOF + INTEREST_RATE + TIME + 1) * VEHICLE_LOAN_AMOUNT
+};
 
+export const toStringFormValues = values => {
   return `Confirmação\n${values
     .map(value => `Campo: ${value.field}, Valor: ${value.value}`)
     .join('\n')}`
-    .concat(`\nTotal ${(IOF + INTEREST_RATE + TIME + 1) * VEHICLE_LOAN_AMOUNT}`)
+    .concat(`\nTotal ${calculationLendingVehicle(values)}`)
 }
 
 export function Send (values) {
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,6 @@ import './header/header.css'
 import './header/__container/header__container.css'
 import './field-group/field-group.css'
 import './field/field.css'
-import './form/__fields/form__fields.css'
-import './range/range.css'
 import './range/__values/range__values.css'
 import './range/__input/range__input.css'
 import './form/__result/form__result.css'
@@ -14,36 +12,8 @@ import './quota/quota.css'
 import './quota/__container/quota__container.css'
 import './button/button.css'
 import Submit from './form/form'
-
-export const getFormValues = formElement =>
-  Object.values(formElement.elements)
-    .filter(element => ['SELECT', 'INPUT'].includes(element.nodeName))
-    .map(element => ({
-      field: element.name,
-      value: element.value
-    }))
-
-export function handleChangeRangeVehicleUnderWarranty (
-  warrantyRangeElement,
-  vehicleWarrantyElement
-) {
-  const MIN_VALUE = 12000.0
-  warrantyRangeElement.addEventListener('change', function (event) {
-    vehicleWarrantyElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
-  })
-}
-
-export function handleChangeVehicleLoanAmount (
-  loanAmountRangeElement,
-  loanAmountElement
-) {
-  const MIN_VALUE = 30000.0
-  loanAmountRangeElement.addEventListener('change', function (event) {
-    loanAmountElement.value =
-      (Number(MIN_VALUE) * Number(event.target.value)) / 100 + Number(MIN_VALUE)
-  })
-}
+import getFormValues from './form/__fields/form__fields'
+import handleChangeRangeUnderMinValue from './range/range'
 
 export default class CreditasChallenge {
   static initialize () {
@@ -52,17 +22,19 @@ export default class CreditasChallenge {
 
   static registerEvents () {
     const formElement = document.querySelector('.form')
-    const fromValues = getFormValues(formElement)  
+    const fromValues = getFormValues(formElement)
     Submit(formElement, fromValues)
 
-    handleChangeRangeVehicleUnderWarranty(
+    handleChangeRangeUnderMinValue(
       document.getElementById('valor-garantia-range'),
-      document.getElementById('valor-garantia')
+      document.getElementById('valor-garantia'),
+      12000.0
     )
 
-    handleChangeVehicleLoanAmount(
+    handleChangeRangeUnderMinValue(
       document.getElementById('valor-emprestimo-range'),
-      document.getElementById('valor-emprestimo')
+      document.getElementById('valor-emprestimo'),
+      30000.0
     )
   }
 }
