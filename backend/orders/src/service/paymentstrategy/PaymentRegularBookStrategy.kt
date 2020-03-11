f9e897ea91556944e875e81fc916049381ae48d7
murilo.santos <murilo.santos@wirecard.com>
2020-03-11 02:08:55 -0300

    Enhancing PaymentRegularBookStrategy functionality

--- backend/orders/src/service/paymentstrategy/PaymentRegularBookStrategy.kt
+++ backend/orders/src/service/paymentstrategy/PaymentRegularBookStrategy.kt
@@ -1,9 +1,14 @@
 package challenge.service.paymentstrategy
 
 import challenge.model.Payment
+import challenge.model.ShippingLabel
 
 class PaymentRegularBookStrategy: PaymentStrategy {
+
+    private val shippingService = ShippingService()
+
     override fun pay(payment: Payment) {
-        println("PaymentRegularBookStrategy")
+        val shippingLabel = ShippingLabel(notification = "Product Free from taxes, according to Constitution Art. 150, VI, d.", order = payment.order)
+        shippingService.shipProduct(shippingLabel)
     }
 }
